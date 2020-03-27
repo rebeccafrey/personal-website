71c4b53f2e8fcaa615fb9a53a1c864fc0af1f88c
rebeccafrey <rebecca.frey101@gmail.com>
2020-03-27 15:01:31 +0100

    Add commit for docs - parcel

--- docs/main.fb6bbcaf.js
+++ docs/main.fb6bbcaf.js
@@ -118,7 +118,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"js/main.js":[function(require,module,exports) {
-console.log("hello world");
+console.log('hello world');
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53841" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59963" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
